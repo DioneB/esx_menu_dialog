c881c644133f52e1540f8d25617ba12e3acdd347
Dione Batista <dione1993@hotmail.com>
2018-09-04 16:10:29 -0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,50 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Menu Dialog'
+
+version '1.0.0'
+
+client_scripts {
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	--CSS
+	'html/css/app.css',
+	'html/css/bootstrap-reboot.min.css.map',
+	'html/css/bootstrap-reboot.min.css',
+	'html/css/bootstrap-reboot.css.map',
+	'html/css/bootstrap-reboot.css',
+	'html/css/bootstrap-grid.min.css.map',
+	'html/css/bootstrap-grid.min.css',
+	'html/css/bootstrap-grid.css.map',
+	'html/css/bootstrap-grid.css',
+	'html/css/bootstrap.min.css.map',
+	'html/css/bootstrap.min.css',
+	'html/css/bootstrap.css.map',
+	'html/css/bootstrap.css',
+	--JS
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+	'html/js/bootstrap.min.js.map',
+	'html/js/bootstrap.min.js',
+	'html/js/bootstrap.js.map',
+	'html/js/bootstrap.js',
+	'html/js/bootstrap.bundle.min.js.map',
+	'html/js/bootstrap.bundle.min.js',
+	'html/js/bootstrap.bundle.js.map',
+	'html/js/bootstrap.bundle.js',
+	--RESTO
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf',
+	'html/fonts/sufrimeda.ttf',
+	'html/fonts/gzc.ttf',
+	'html/img/cursor.png',
+	'html/img/keys/enter.png',
+	'html/img/keys/return.png',
+}
